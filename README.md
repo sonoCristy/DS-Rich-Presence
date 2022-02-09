22cc87ad56d69ff04454083069634ac2fac99d4c
Cristy <61757856+Cristy-html@users.noreply.github.com>
2022-02-09 19:32:58 +0100

    Small Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+[![DS+RichPresence](https://i.imgur.com/39u62Fp.png)](https://i.imgur.com/39u62Fp.png) 
+
 # DS Rich Presence
+
+Ad at your Discord Account **Discord Rich Presence Preset** for costumize your profile.
+
+Creating by sonoCristy
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// This template was made by FusionTerror.
-const DiscordRPC = require('discord-rpc'); // Requiring the discord-rpc package.
-const client = new DiscordRPC.Client({ transport: 'ipc' }); // Creating a client variable with is our rpc client.
-require('dotenv').config(); // Requiring the content of our .env file.
+// This template was made by sonoCristy.
+const DiscordRPC = require('discord-rpc');
+const client = new DiscordRPC.Client({ transport: 'ipc' });
+require('dotenv').config();
 
 (async () => {
-    client.on('ready', async () => { // Calling the ready event.
-        await client.setActivity({ // Setting the Rich Presence Activity based on what is passed in here.
-            buttons: [{ label: "Immettere Nome", url: "Immettere Link" }], //per aggiungere un bottone copiare da dalla prima parentesi { fino all'ultima } mettendo una virgola , in mezzo così da lasciare spazio
+    client.on('ready', async () => {
+        await client.setActivity({
+            buttons: [{ label: "Immettere Nome", url: "Immettere Link" }], // Per aggiungere un bottone copiare da dalla prima parentesi { fino all'ultima } mettendo una virgola , in mezzo così da lasciare spazio
             details: "Name",
             largeImageKey: "Name of large image",
             largeImageText: "Text in large image",
@@ -17,5 +17,5 @@ require('dotenv').config(); // Requiring the content of our .env file.
         console.log("Discord Rich Presence has been applicated.");
     });
 
-    await client.login({ clientId: process.env.applicationID }).catch(console.error); // Logging into our application.
+    await client.login({ clientId: process.env.applicationID }).catch(console.error);
 })();
